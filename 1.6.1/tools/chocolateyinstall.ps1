45d829d2515bbdf09652fcd83631e0082aa6b16f
paul.kling <paul.kling@protolabs.com>
2015-10-29 14:24:40 -0500

    added ability to set path

--- 1.6.1/tools/chocolateyinstall.ps1
+++ 1.6.1/tools/chocolateyinstall.ps1
@@ -1,5 +1,8 @@
 ﻿$esPath = "c:\elasticsearch"
+$additionalInstallArgs = $env:chocolateyInstallArguments;
+if ($additionalInstallArgs -eq $null) { $additionalInstallArgs = $esPath; }	
+	
 $download = "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.6.1.zip"
 $sha = "3767e802de396f0b648710d5367779c5e5e534a5"
 
-Install-ChocolateyZipPackage 'elasticsearch-1.6.1' $download $esPath -checksum $sha -checksumType 'sha1'
+Install-ChocolateyZipPackage 'elasticsearch-1.6.1' $download $additionalInstallArgs -checksum $sha -checksumType 'sha1'
--- 1.7.1/tools/chocolateyinstall.ps1
+++ 1.7.1/tools/chocolateyinstall.ps1
@@ -1,5 +1,8 @@
 ﻿$esPath = "c:\elasticsearch"
+$additionalInstallArgs = $env:chocolateyInstallArguments;
+if ($additionalInstallArgs -eq $null) { $additionalInstallArgs = $esPath; }	
+
 $download = "http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.zip"
 $sha = "89b50d4958a8f6e8d7e3359fa27e94eda525eadc"
 
-Install-ChocolateyZipPackage 'elasticsearch-1.7.1' $download $esPath -checksum $sha -checksumType 'sha1'
+Install-ChocolateyZipPackage 'elasticsearch-1.7.1' $download $additionalInstallArgs -checksum $sha -checksumType 'sha1'
